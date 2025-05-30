2f3fd5ccc6e44dc2944cbe7c54032057d0213ddf
PradeepPs04 <pradeep.ps2004@gmail.com>
2025-05-30 10:00:46 +0530

    fix: add page title and favicon

--- client/src/components/auth/Layout.jsx
+++ client/src/components/auth/Layout.jsx
@@ -5,9 +5,10 @@ const AuthLayout = () => {
   return (
     <div className='flex min-h-screen w-full'>
         <div className='hidden lg:flex items-center justify-center bg-black w-1/2 px-12'>
-            <div className='max-w-md space-y-6 text-center text-white'>
-                <h1 className='text-4xl font-extrabold tracking-tight'>
-                    Welcome to ECommerce Shopping
+            <div className='space-y-6 text-center text-white'>
+                <h1 className='text-4xl font-extrabold tracking-tight flex flex-col'>
+                    Welcome to 
+                    <span className='mt-2 text-5xl text-[#95A6D5]'>🐾Petmart Royle🐾</span>
                 </h1>
             </div>
         </div>
